c69fbcc718a27320a8fadf000c0b6ca0b66ab404
David Mendez <desiratech@gmail.com>
2020-06-29 22:50:11 -0500

    forgot the new component lol

---
+++ src/js/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <div className="Header">
+        <h1>Dear Movie Diary</h1>
+        <h3>A place to store your favorite movies!</h3>
+      </div>
+    </React.Fragment>
+  )
+}
+export default Header
